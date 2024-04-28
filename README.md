8e0c5dc47778fa74046ac232f2b5239212105b2e
Charles <charlesmuteithia@gmail.com>
2024-04-29 00:24:50 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+
+# The Royal Bank of Flatiron
+
+The Royal  Bank of Flatiron is a React application that displays a list of your recent bank transactions, among other features. It involves fetching data from a local server running JSON DB server.
+
+
+## Authors
+
+- [@githinjisamson1](https://www.github.com/octokatherine)
+
+
+## Contributing
+
+Contributions are always welcome!
+
+
+
+## Features
+
+- Table of transactions.
+- Fill out and submit the form to add a new transaction.
+- Post the new transaction to the backend API for persistence.
+- Filter transactions by typing into the search bar.
+
+
+## Installation
+
+```bash
+  copy ssh
+  git clone phase-2-week-1-bank-of-flatiron
+  cd phase-2-week-1-bank-of-flatiron
+  json-server --watch db.json --port 8000
+  open using an editor e.g., vs code: npm start
+  open the application using your favorite browser
+```
+    
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+## Feedback
+
+If you have any feedback, please reach out to me at githinjisamson148@gmail.com
+
+
+## Lessons Learned
+
+- Maintaining state at top level component
+- useState hook usage
+- useEffect hook usage
+- passing props
+- conditional rendering
+- modular css
+
+
+## Tech Stack
+
+**Client:** ```HTML | CSS | JS | REACT```
+
+**Version Control:** ```Git```
+
+**Server:** ```json-server```
+
+
+## Used By
+
+For learning purposes :)
+
+# phase-2-week-1-bank-of-flatiron
+# phase-2-week-1-bank-of-flatiron
---
+++ src/App.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from "react";
+import Header from "./components/header/Header";
+import Search from "./components/search/Search";
+import Table from "./components/table/Table";
+import AddNewTransaction from "./components/addNewTransaction/AddNewTransaction";
+
+const App = () => {
+  // !maintain all states at top level/App
+  const [transactions, setTransactions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [formData, setFormData] = useState({
+    date: "",
+    description: "",
+    category: "",
+    amount: "",
+  });
+
+  // for searching based on category
+  const [filter, setFilter] = useState("");
+
+  // fetch API - all
+  const fetchTransactions = () => {
+    fetch("http://localhost:5501/transactions")
+      .then((response) => {
+        // readableStream to json
+        return response.json();
+      })
+      .then((data) => {
+        // update state
+        setTransactions(data);
+
+        // user-experience/show page after 3 sec
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1000);
+      })
+      .catch((err) => {
+        // fetch error
+        console.log(err.message);
+      });
+  };
+
+  // run useEffect on initial render
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  // useEffect(() => {
+  //   postTransaction();
+  // }, [transactions]);
+
+  // signify fetching
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  // !handling input change
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    // updating formData using dynamic object properties
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // !upon form submission
+  const handleFormSubmit = (e) => {
+    // prevent default refresh behavior
+    e.preventDefault();
+
+    // POST transaction/add Item to transactions
+    fetch("http://localhost:5501/transactions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setTransactions([...transactions, data]);
+      })
+      .catch();
+
+    // TODO: clear form values upon form submission
+    // !!will change controlled input
+    setFormData({
+      date: "",
+      description: "",
+      category: "",
+      amount: "",
+    });
+  };
+
+  // !handle searching recentTransactions
+  const handleFilter = (e) => {
+    setFilter(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Search filter={filter} handleFilter={handleFilter} />
+      <AddNewTransaction
+        formData={formData}
+        handleChange={handleChange}
+        handleFormSubmit={handleFormSubmit}
+      />
+      {/* pass transactions as prop */}
+      <Table transactions={transactions} filter={filter} />
+    </div>
+  );
+};
+
+
+export default App;
